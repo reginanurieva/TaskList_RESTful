c4b8cb16a7a74a5de8180672143afff1056696e0
Chris Crow and Regina Nurieva <regina@>
2018-09-11 09:53:32 -0700

    finish practice project.

---
+++ TaskList/Controllers/ItemsController.cs
@@ -0,0 +1,34 @@
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using TaskList.Models;
+
+namespace TaskList.Controllers
+{
+    public class ItemsController : Controller
+    {
+
+        [HttpGet("/items")]
+        public ActionResult Index()
+        {
+            List<Item> allItems = Item.GetAll();
+            return View(allItems);
+        }
+
+
+        [HttpGet("/items/new")]
+        public ActionResult CreateForm()
+        {
+            return View();
+        }
+
+        [HttpPost("/items")]
+        public ActionResult Create()
+        {
+            Item newItem = new Item(Request.Form["new-item"]);
+            newItem.Save();
+            List<Item> allItems = Item.GetAll();
+            return View("Index", allItems);
+        }
+    }
+}
