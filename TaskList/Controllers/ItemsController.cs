6ad1810c3257945b4a152fd62103ea10cacacfa3
Regina Nurieva <regginurieva@gmail.com>
2018-09-12 09:27:19 -0700

    add files

--- TaskList/Controllers/ItemsController.cs
+++ TaskList/Controllers/ItemsController.cs
@@ -1,34 +1,67 @@
 using Microsoft.AspNetCore.Mvc;
-using System;
 using System.Collections.Generic;
 using TaskList.Models;
+using System;
 
 namespace TaskList.Controllers
 {
     public class ItemsController : Controller
     {
-
-        [HttpGet("/items")]
+      [HttpGet("/items")]
         public ActionResult Index()
         {
             List<Item> allItems = Item.GetAll();
             return View(allItems);
         }
 
-
         [HttpGet("/items/new")]
         public ActionResult CreateForm()
         {
             return View();
         }
-
         [HttpPost("/items")]
         public ActionResult Create()
         {
-            Item newItem = new Item(Request.Form["new-item"]);
-            newItem.Save();
-            List<Item> allItems = Item.GetAll();
-            return View("Index", allItems);
+          Item newItem = new Item(Request.Form["new-item"]);
+          newItem.Save();
+          List<Item> allItems = Item.GetAll();
+          return View("Index", allItems);
         }
+
+        [HttpPost("/items/delete")]
+        public ActionResult DeleteAll()
+        {
+            Item.ClearAll();
+            return View();
+        }
+
+        [HttpGet("/items/{id}")]
+        public ActionResult Details(int id)
+        {
+            Item item = Item.Find(id);
+            return View(item);
+        }
+        // [HttpGet("/items")]
+        // public ActionResult Index()
+        // {
+        //     List<Item> allItems = Item.GetAll();
+        //     return View(allItems);
+        // }
+        //
+        //
+        // [HttpGet("/items/new")]
+        // public ActionResult CreateForm()
+        // {
+        //     return View();
+        // }
+        //
+        // [HttpPost("/items")]
+        // public ActionResult Create()
+        // {
+        //     Item newItem = new Item(Request.Form["new-item"]);
+        //     newItem.Save();
+        //     List<Item> allItems = Item.GetAll();
+        //     return View("Index", allItems);
+        // }
     }
 }
